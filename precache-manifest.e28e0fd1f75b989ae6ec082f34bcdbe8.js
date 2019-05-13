283cac23bf3a14a353a4bff03ddd6032f2ee1207
Emmanuel Eze <emereuwaonueze@gmail.com>
2019-05-13 13:40:29 +0100

    Updates

---
+++ precache-manifest.e28e0fd1f75b989ae6ec082f34bcdbe8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8fbe6216b3cbb45f02eafa5b042269e",
+    "url": "/skype-clone/index.html"
+  },
+  {
+    "revision": "0362c7994cfe83e77270",
+    "url": "/skype-clone/static/css/main.1f985c8e.chunk.css"
+  },
+  {
+    "revision": "91688fe6f84fb20ef67a",
+    "url": "/skype-clone/static/js/2.78da4948.chunk.js"
+  },
+  {
+    "revision": "0362c7994cfe83e77270",
+    "url": "/skype-clone/static/js/main.09a4f703.chunk.js"
+  },
+  {
+    "revision": "b3a5573655d86bc79b82",
+    "url": "/skype-clone/static/js/runtime~main.9f1663f3.js"
+  }
+]);
